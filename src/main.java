10c14f089cdc6ff74dae82fbeacf02754d118591
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-05-05 16:58:07 -0500

    pracitced fizzbuzz in java

--- src/main.java
+++ src/main.java
@@ -1,8 +1,21 @@
 package src;
 
+import java.util.Scanner;
+
 public class main {
     
    public static void main(String[] args) {
        System.out.println("The app is running");
+       int n = 15;
+       for(int num = 0; num <= n; num++) {
+           if(num % 15 == 0)
+           System.out.println("FizzBuzz");
+           else if(num % 3 == 0)
+           System.out.println("Fizz");
+           else if(num % 5 == 0)
+           System.out.println("Buzz");
+           else
+           System.out.println(num);
+       }
    }
 }
