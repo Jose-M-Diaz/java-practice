3e3fa511ddc43d50f5100289d2dfdf78c21a3859
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-05-05 15:53:25 -0500

    added jUnit test class

---
+++ src/BankingTest.java
@@ -0,0 +1,21 @@
+package src;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BankingTest {
+    @Test
+    public void deposit() throws Exception {
+
+    }
+    
+    @Test
+    public void withdraw() throws Exception {
+
+    }
+
+    @Test
+    public void getBalance() throws Exception {
+        
+    }
+}
