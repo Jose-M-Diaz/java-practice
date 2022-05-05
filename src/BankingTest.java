9aa247f80d2d6366e53582216f88faa7c85f7bdd
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-05-05 16:22:42 -0500

    added tests for methods

--- src/BankingTest.java
+++ src/BankingTest.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 public class BankingTest {
     @Test
     public void deposit() throws Exception {
-
+            Banking account = new Banking("Jose", "Diaz", 1000.00);
+            double balance = account.deposit(200.00, true);
+            assertEquals(1200.00, balance, 0);
     }
     
     @Test
@@ -15,7 +17,16 @@ public class BankingTest {
     }
 
     @Test
-    public void getBalance() throws Exception {
-        
+    public void getBalance_deposit() throws Exception {
+        Banking account = new Banking("Jose", "Diaz", 1000.00);
+            account.deposit(200.00, true);
+            assertEquals(1200.00, account.getBalance(), 0);
+    }
+
+    @Test
+    public void getBalance_withdraw() throws Exception {
+        Banking account = new Banking("Jose", "Diaz", 1000.00);
+            account.withdraw(200.00, true);
+            assertEquals(800.00, account.getBalance(), 0);
     }
 }
--- src/main.java
+++ src/main.java
@@ -3,6 +3,6 @@ package src;
 public class main {
     
    public static void main(String[] args) {
-       
+       System.out.println("The app is running");
    }
 }
